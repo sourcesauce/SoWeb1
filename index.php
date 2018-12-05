f7877987b0fb03fd99b5bc34cc2260062060cd81
sourcesauce <sshlc940503@naver.com>
2018-12-05 21:36:26 +0900

    css_repair

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<?php $topTitle = "So의 Web세상"?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 
@@ -11,31 +10,32 @@
 <body>
   <div class="grid_body">
 
-    <div class="grid_body_item">
-      <a id="top_a" href="index.php"> <?php echo "$topTitle"; ?></a>
+    <div class="grid_body_item1">
+      <a id="top_a" href="index.php">So의 Web세상</a>
     </div>
 
-    <div class="grid_body_item">
-      <div class="grid_menu">
-        <div class="grid_menu_item">
-          <a href="main_page.php?id=userdrip">유저드립</a>
+    <div class="grid_body_item2">
+        <div>
+          <a href="main_page.php?id=userdrip">유저업로드</a>
         </div>
-        <div class="grid_menu_item">
+        <div>
           <a href="main_page.php?id=broadcasting">방송연예</a>
         </div>
-        <div class="grid_menu_item">
-          <a href="main_page.php?id=anonymous">익명채팅</a>
+        <div>
+          <a href="main_page.php?id=anonymous">사진</a>
         </div>
-        <div class="grid_menu_item">
+        <div>
           <a href="bookmark.php">북마크</a>
         </div>
       </div>
-    </div>
 
-    <div class="grid_body_item">
-      <p>index 페이지 입니다.</p>
-      <p>총 4개의 페이지로 구성되어있고</p>
-      <p>유저드립, 방송연예, 익명채팅, 북마크로 이루어져있습니다.</p>
+    <div class="grid_body_item3">
+      <div class="grid_main">
+        <div class="grid_main_item">
+
+        </div>
+
+      </div>
     </div>
   </div>
 
