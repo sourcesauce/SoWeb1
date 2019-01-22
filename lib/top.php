3d611cc450a2f215acb9ca6ab9104501cddcf26f
소상훈 <sshlc940503@naver.com>
2019-01-22 10:57:23 +0900

    메뉴 정리

--- lib/top.php
+++ lib/top.php
@@ -4,9 +4,8 @@
 
 <nav id="top_menu">
   <ul>
-    <li><a class="menuLink" href="user.php" target="_self">유저업로드</a></li>
-    <li><a class="menuLink" href="tv.php" target="_self">방송연예</a></li>
-    <li><a class="menuLink" href="testPage.html" target="_self">사진</a></li>
+    <li><a class="menuLink" href="user.php" target="_self">게시판</a></li>
+    <li><a class="menuLink" href="testPage.html" target="_self">TEST</a></li>
     <li><a class="menuLink" href="testPage.html" target="_self">북마크</a></li>
   </ul>
 </nav>
